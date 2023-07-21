53e3bb4a117a3fd0d707e9f146f5ea0a50edb22f
“Sindhura <sindhurapogarthi@gmail.com>
2023-07-21 23:00:17 +0530

    Read functionality of database done

--- src/components/Feed/Demo.js
+++ src/components/Feed/Demo.js
@@ -1,33 +1,64 @@
-import React, { useState } from 'react';
-import {ref,push, set } from "firebase/database";
-import {database} from '../../firebase'
+import React, { useState, useEffect } from "react";
+import { ref, push, set, onValue, off } from "firebase/database";
+import { database } from "../../firebase";
 
 export default function Demo() {
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [userData, setUserData] = useState(null);
 
-    const handlechange = () => {
+  const handlechange = () => {
+    const db = database;
+    const usersRef = ref(db, "users/");
+    const newUserRef = push(usersRef);
+
+    set(newUserRef, {
+      Name: name,
+      Age: age,
+    });
+  };
+
+  useEffect(() => {
+    
+    const db = database;
+    const id = "-N_tN34MlA2D8O-KzK4x"
+
+    const useref = ref(db, `users/${id}`);
+    onValue(useref, (snapshot) => {
+      const data = snapshot.val();
+      setUserData(data);
       
-        const db = database;
-        const usersRef = ref(db, 'users/');
-        const newUserRef = push(usersRef); // Generate a unique key for the new user
+      console.log(data);
+    });
+  }, []);
 
-        set(newUserRef, {
-             Name: name,
-             Age: age,
-            });
-            
-   
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="text"
+        value={age}
+        onChange={(e) => {
+          setAge(e.target.value);
+        }}
+      />
+      <br />
+      <button onClick={handlechange}>Save details</button>
 
-    }
-
-    return (
+      {userData && (
         <div>
-            <input type='text' value={name} onChange={(e) => { setName(e.target.value) }}></input>
-            <br />
-            <input type='text' value={age} onChange={(e) => { setAge(e.target.value) }}></input>
-            <br />
-            <button onClick={handlechange}>Save details</button>
+          <h2>User Data:</h2>
+          <p>Name: {userData.Name}</p>
+          <p>Age: {userData.Age}</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
