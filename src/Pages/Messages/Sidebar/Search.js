4d506af35ff2cf2817ab9f6e83d6fc80f0735a50
“Sindhura <sindhurapogarthi@gmail.com>
2023-07-26 14:53:53 +0530

    fixing the searching functionality

--- src/Pages/Messages/Sidebar/Search.js
+++ src/Pages/Messages/Sidebar/Search.js
@@ -1,96 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import styles from './Serach.module.css';
-import image from '../../../Assets/images/wllpaper2.webp';
-import { collection, query, where, onSnapshot, getDocs, setDoc,doc, updateDoc, serverTimestamp, getDoc } from 'firebase/firestore';
-import { db } from '../../../firebase';
-import { auth } from '../../../firebase';
-
+import React, { useState, useEffect } from "react";
+import styles from "./Serach.module.css";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  getDocs,
+  setDoc,
+  doc,
+  serverTimestamp,
+  getDoc,
+  orderBy
+} from "firebase/firestore";
+import { db } from "../../../firebase";
+import { auth } from "../../../firebase";
 
 export default function Search() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState(""); 
   const [users, setUsers] = useState([]);
-  const [allUsers, setAllUsers] = useState([])
 
   useEffect(() => {
-    const unsubscribe = listenForUsers(); 
-    return () => unsubscribe(); 
+    const unsubscribe = listenForUsers();
+    return () => unsubscribe();
   }, []);
 
-  const handlechange=(e)=>{
+  const handlechange = (e) => {
     const inputValue = e.target.value.toLowerCase();
-      console.log(allUsers)
+    setUsername(inputValue);
 
-      const filteredUsers = allUsers?.filter((item) => item.name.toLowerCase().includes(inputValue));
-      console.log(filteredUsers);
-      setUsers(filteredUsers);
-  }
+    const filteredUsers = users?.filter((item) =>
+      item.name.toLowerCase().includes(inputValue)
+    );
+
+    setUsers(filteredUsers);
+  };
 
   const listenForUsers = () => {
-   
-    const q = query(collection(db, 'users'), where('name', '>=', username)); 
+    console.log(auth.currentUser)
+    const q = query(collection(db, "users"), where("name", ">=", username), orderBy("name"),where("name", "!=", auth.currentUser.displayName));
     return onSnapshot(q, (querySnapshot) => {
-      const updatedUsers = querySnapshot.docs
-        .map((doc) => doc.data())
-        .filter((user) => user.name.toLowerCase().includes(username.toLowerCase())); 
-          setAllUsers(updatedUsers)
-          setUsers(updatedUsers)
+      const updatedUsers = querySnapshot.docs.map((doc) => doc.data());
+      setUsers(updatedUsers);
     });
   };
 
-  const handleselect=async(user)=>{
-    const myuser = auth.currentUser;
-    const combinedId= myuser.uid > user.uid? myuser.uid + user.uid: user.uid+myuser.uid
-    console.log(user.uid)
-    console.log(user.name)
-    console.log(user)
-    const res= await getDoc(doc(db,'chats',combinedId))
-    if(!res.exists()){
-      setDoc(doc(db, 'chats', combinedId), { messages: [] });
 
-      await setDoc(doc(db, 'userchats', user.uid), {
-        [combinedId]:{
-          userInfo:
-          {
-             uid: user.uid,
-             displayName: user.name,
-             photoURL: user.photoURL
-           },
-           date: serverTimestamp()
-         
-       }
+  const handleselect = async (user) => {
+    const myuser = auth.currentUser;
+    const combinedId =
+      myuser.uid > user.uid ? myuser.uid + user.uid : user.uid + myuser.uid;
+
+    const res = await getDoc(doc(db, "chats", combinedId));
+    if (!res.exists()) {
+      setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+      await setDoc(doc(db, "userchats", user.uid), {
+        [combinedId]: {
+          userInfo: {
+            uid: user.uid,
+            displayName: user.name,
+            photoURL: user.photoURL,
+          },
+          date: serverTimestamp(),
+        },
       });
-      await setDoc(doc(db, 'userchats', myuser.uid), {
+      await setDoc(doc(db, "userchats", myuser.uid), {
         [combinedId]: {
           userInfo: {
             uid: myuser.uid,
             displayName: myuser.displayName,
-            photoURL: myuser.photoURL
+            photoURL: myuser.photoURL,
           },
-          date: serverTimestamp()
-        }
+          date: serverTimestamp(),
+        },
       });
-      
     }
+    const q = query(collection(db, "users"));
+    const querySnapshot = await getDocs(q);
+    const updatedUsers = querySnapshot.docs.map((doc) => doc.data());
+    const filteredUsers = updatedUsers.filter((item) => item.uid !== myuser.uid);
+  
+   
+    setUsers(filteredUsers);
+    setUsername("");
+    
+  };
 
-
-  }
-
-  const userUrl = 'https://img.icons8.com/ios-filled/50/user-male-circle.png';
+  const userUrl = "https://img.icons8.com/ios-filled/50/user-male-circle.png";
 
   return (
     <div className={styles.serach}>
       <div className={styles.searchform}>
         <input
-          type='text'
-          placeholder='Find a user....'
+          type="text"
+          placeholder="Find a user...."
           onChange={handlechange}
+          value={username}
         ></input>
-     
       </div>
       {users.map((user) => (
-       
-        <div className={styles.userchat} key={user.uid} onClick={() => handleselect(user)}>
-          <img src={user.photoURL ? user.photoURL : userUrl} alt='myimage'></img>
+        <div
+          className={styles.userchat}
+          key={user.uid}
+          onClick={() => handleselect(user)}
+        >
+          <img
+            src={user.photoURL ? user.photoURL : userUrl}
+            alt="myimage"
+          ></img>
           <div className={styles.userchatinfo}>
             <span>{user.name}</span>
           </div>
